f4ea63d5d41736244fdc612895390ee773bd5aef
ashutosh0203 <mishrashutosh18@gmail.com>
2026-06-22 18:05:32 +0530

    Add Redis as a dependency and implement Redis client connection

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,12 +1,23 @@
 import  Express  from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
+import {createClient} from 'redis';
 
 const app = Express();
 
 dotenv.config();
 connectDb();
 
+export const redisClient = createClient({
+    url: process.env.REDIS_URL!,
+});
+
+redisClient.connect().then(()=>{
+    console.log("Connected to Redis");
+}).catch((err)=>{
+    console.error("Error connecting to Redis:", err);
+    process.exit(1);
+});
 
 const port = process.env.PORT || 5000;
 
