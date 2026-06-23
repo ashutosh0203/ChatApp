011835a477705e9fc264343ca9b8aca973d7eb73
ashutosh0203 <mishrashutosh18@gmail.com>
2026-06-23 23:48:37 +0530

    Add JWT authentication: implement token generation and user verification endpoints

--- backend/user/src/controllers/user.ts
+++ backend/user/src/controllers/user.ts
@@ -1,5 +1,7 @@
 import tryCatch from "../config/TryCatch.js";
 import { redisClient } from "../index.js";
+import {User} from "../model/User.js";
+import { generateToken } from "../config/generateToken.js";
 
 import { publishToQueue } from "../config/rabbitmq.js";
 
@@ -27,4 +29,39 @@ export const loginUser = tryCatch(async (req, res) => {
     await publishToQueue("send-otp", message);
 
     res.status(200).json({ message: "OTP sent successfully."})
+})
+
+export const verifyUser= tryCatch(async (req, res) => {
+    // console.log("verifyUser called with body:", req.body);
+    const { email, otp:enteredOtp } = req.body;
+
+    if(!email || !enteredOtp){
+        res.status(400).json({message: "Email and OTP are required."});
+        return;
+    }
+
+    const otpKey = `otp:${email}`;
+    const storedOtp = await redisClient.get(otpKey);
+
+
+    if(!storedOtp || storedOtp !== enteredOtp){
+        res.status(400).json({message: "OTP has expired or is invalid."});
+        return;
+    }
+
+    await redisClient.del(otpKey); // Delete the OTP after successful verification
+
+    let user = await User.findOne({ email });
+
+    if(!user){
+        const name = email.slice(0,8); // Extract the first 8 characters of the email as the name
+        user = await User.create({ name,email });
+    }
+    const token = generateToken(user);
+
+    res.json({
+        message: "User verified successfully.",
+        token,
+        user
+    })
 })
